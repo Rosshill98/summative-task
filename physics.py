b49969a536a1e328e351db1975b52147788c31e2
Ross Hill <rosshill@protonmail.com>
2016-05-24 12:10:38 -0400

    making a seperate graphing class

--- physics.py
+++ physics.py
@@ -1,8 +1,9 @@
 from rocket import rocket
+from graph import graph
 class main:
     def start(self):
         specs = rocket().getSpecs()
         variables = rocket().deriveVectors(specs)
-        rocket().drawGraph(variables,specs)
+        graph().drawGraph(variables,specs)
 
 main().start() #initializes the program
